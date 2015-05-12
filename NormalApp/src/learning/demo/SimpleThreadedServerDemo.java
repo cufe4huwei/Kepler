2d39acd475f9eeee17e97c5bd67c9de4943f8a73
hugo <weigeriji@163.com>
2015-05-12 18:06:43 +0800

    Add threaded server

--- NormalApp/src/learning/demo/SimpleThreadedServerDemo.java
+++ NormalApp/src/learning/demo/SimpleThreadedServerDemo.java
@@ -4,15 +4,31 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class SimpleServerDemo {
+public class SimpleThreadedServerDemo {
 	public static void main(String... args) throws IOException{
 		ServerSocket server = new ServerSocket(8080);
 		Utils.log("An instance of server socket is created");
 		while(true){
 			Utils.log("Waiting for new client");
 			Socket socket = server.accept(); //blocking call, never null
-			Utils.process(socket);
+			Thread t = new Thread(new SocketProcessImpl(socket));
+			t.start();
 		}
+	}
+	
+	private static class SocketProcessImpl implements Runnable {
+		private Socket client = null;
+		public SocketProcessImpl(Socket client) {
+			super();
+			this.client = client;
+		}
+		
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			Utils.process(client);
+		}
+		
 	}
 
 	
--- NormalApp/src/learning/demo/Utils.java
+++ NormalApp/src/learning/demo/Utils.java
@@ -1,5 +1,9 @@
 package learning.demo;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class Utils {
 	public static int transmogrify(int src){
 		if(Character.isLetter(src)){
@@ -11,5 +15,41 @@ public class Utils {
 	
 	public static void log(String format, Object... args) {
 		System.out.println(String.format(format, args));
+	}
+	
+	public static void process(Socket socket) {
+		String clientID = socket.getInetAddress().getCanonicalHostName();
+		Utils.log("A client of %s is acceptted", clientID);
+		InputStream input = null;
+		OutputStream output = null;
+		try {
+			input = socket.getInputStream();
+			output = socket.getOutputStream();
+			int data;
+			while((data = input.read()) != -1) {
+				data = Utils.transmogrify(data);
+				output.write(data);
+			}
+		} catch(Exception e){
+			Utils.log("Error during interact with client, " + e.getMessage());
+		}
+		finally {
+			if(input != null){
+				try {
+					Utils.log("Close input stream of %s", clientID);
+					input.close();
+				} catch (Exception e) {
+					Utils.log("Error when closing input stream");
+				}
+			}
+			if(output != null){
+				try {
+					Utils.log("Close out stream of %s", clientID);
+					output.close();
+				} catch (Exception e) {
+					Utils.log("Error when closing output stream");
+				}
+			}
+		}
 	}
 }
